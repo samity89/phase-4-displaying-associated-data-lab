835bb61f9ca453f1aac30b1cf64be68baba39e47
Samuel Amity <sam.amity@gmail.com>
2023-05-08 12:09:08 -0400

    lesson completed' )

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def show
+        user = find_user
+        render json: user, include: :items
+    end
+
+    private
+
+    def find_user
+        User.find_by(id: params[:id])
+    end
 end
